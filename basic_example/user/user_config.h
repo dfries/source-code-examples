a643b073a9c4ec8ac8cb0b3e97716fd5e3696eaa
David Fries <David@Fries.net>
2016-12-21 11:23:50 -0600

    modify example for WPA2-EAP test
    This is using ESP8266_NONOS_SDK_V2.0.0_16_08_10
    
    heap 48616 line 61
    WPA2 ENTERPRISE VERSION: [v2.0] enable
    heap 48248 line 66
    mode : sta(5c:cf:7f:88:b3:d4)
    add if0
    scandone
    state: 0 -> 2 (b0)
    state: 2 -> 3 (0)
    state: 3 -> 5 (10)
    add 0
    aid 1
    cnt
    Method private structure allocated failure
    state: 5 -> 2 (17c0)
    rm 0
    heap 47056 wifi status 1
    heap 47056 wifi status 1
    heap 47056 wifi status 1
    heap 47056 wifi status 1
    still in connecting, disconnect and try again
    state: 2 -> 0 (0)
    scandone
    state: 0 -> 2 (b0)
    state: 2 -> 3 (0)
    state: 3 -> 5 (10)
    add 0
    aid 1
    cnt
    Method private structure allocated failure
    state: 5 -> 2 (17c0)
    rm 0
    heap 47000 wifi status 1
    heap 47000 wifi status 1
    heap 47000 wifi status 1
    heap 47000 wifi status 1

--- basic_example/user/user_config.h
+++ basic_example/user/user_config.h
@@ -1,2 +1,4 @@
 #define SSID "Your SSID"
 #define SSID_PASSWORD "Your Password Here"
+#define EAP_USER "eap_username"
+#define EAP_PASSWORD "eap_password"
--- basic_example/user/user_main.c
+++ basic_example/user/user_main.c
@@ -5,18 +5,35 @@
 #include "user_config.h"
 #include "user_interface.h"
 
+#include <wpa2_enterprise.h>
+
 #define user_procTaskPrio        0
 #define user_procTaskQueueLen    1
 os_event_t    user_procTaskQueue[user_procTaskQueueLen];
 static void loop(os_event_t *events);
 
-//Main code function
 static void ICACHE_FLASH_ATTR
-loop(os_event_t *events)
+print_heap(os_event_t *events)
 {
-    os_printf("Hello\n\r");
-    os_delay_us(10000);
-    system_os_post(user_procTaskPrio, 0, 0 );
+    int status = wifi_station_get_connect_status();
+    os_printf("heap %u wifi status %u\n", system_get_free_heap_size(), status);
+    if(status == STATION_IDLE)
+    {
+        os_printf("idle, try connecting\n");
+        wifi_station_connect();
+    }
+    static int connect_count;
+    if(status == STATION_CONNECTING)
+    {
+        ++connect_count;
+        if(connect_count == 4)
+        {
+            os_printf("still in connecting, disconnect and try again\n");
+            wifi_station_disconnect();
+            wifi_station_connect();
+            connect_count = 0;
+        }
+    }
 }
 
 //Init function 
@@ -27,16 +44,29 @@ user_init()
     char password[64] = SSID_PASSWORD;
     struct station_config stationConf;
 
+    // default appears to be a non-standard 74880 baud
+    uart_div_modify(0, UART_CLK_FREQ / 115200);
+
     //Set station mode
-    wifi_set_opmode( 0x1 );
+    wifi_set_opmode(STATION_MODE);
 
     //Set ap settings
+    // skip station config, expect the station is already setup in flash
+    #if 0
     os_memcpy(&stationConf.ssid, ssid, 32);
     os_memcpy(&stationConf.password, password, 64);
     wifi_station_set_config(&stationConf);
+    #endif
 
-    //Start os task
-    system_os_task(loop, user_procTaskPrio,user_procTaskQueue, user_procTaskQueueLen);
+    os_printf("heap %u line %u\n", system_get_free_heap_size(), __LINE__);
+    wifi_station_set_wpa2_enterprise_auth(true);
+    wifi_station_set_reconnect_policy(false);
+    wifi_station_set_enterprise_username((u8*)EAP_USER, os_strlen(EAP_USER));
+    wifi_station_set_enterprise_password((u8*)EAP_PASSWORD, os_strlen(EAP_PASSWORD));
+    os_printf("heap %u line %u\n", system_get_free_heap_size(), __LINE__);
 
-    system_os_post(user_procTaskPrio, 0, 0 );
+    static os_timer_t heap_timer;
+    os_timer_disarm(&heap_timer);
+    os_timer_setfn(&heap_timer, &print_heap, NULL);
+    os_timer_arm(&heap_timer, 3000, 1);
 }
